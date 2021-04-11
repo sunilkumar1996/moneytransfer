57b5d9a3307547c32da5bdacb7e6eb303885f42e
sunilkumar1996 <sunilrajputdev@gmail.com>
2021-04-11 17:26:07 +0530

    minor change

--- moneytransfer/settings.py
+++ moneytransfer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ptscnc+y!fl!qj204zj#e@%#fkvf#mm$uv6=1a&hz8qgkz!v9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
